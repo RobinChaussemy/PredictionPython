7d3ebe74910b02307e02d7d14480ddd5d0cae9c4
RobinChaussemy <chaussemy.robin@gmail.com>
2024-01-04 10:47:23 +0100

    Fix path

--- R/KNN.R
+++ R/KNN.R
@@ -22,7 +22,7 @@ Int <- function(x){
 #' @examples KNN_Python(c("Ama Dablam","Autumn","France","Climber",2025,1,1,1,1,1,1,1), n = 11 , p = 3)
 
 KNN_Python <- function(indiv_prediction,n = Int(11), p = Int(3)){
-  source_python("Python/main.py")
+  source_python("../Python/main.py")
   KNN_result = KNN_Process(data,indiv_prediction,target ,Int(n), Int(p))
   return(list(prediction = KNN_result[[1]], proba_f = KNN_result[[2]],proba_s = KNN_result[[3]],temps = KNN_result[[4]] ))
 }
