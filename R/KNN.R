03d3d4298933912060b55ccd22abdf2c7efceccf
RobinChaussemy <chaussemy.robin@gmail.com>
2024-01-05 11:35:48 +0100

    Fix

--- R/KNN.R
+++ R/KNN.R
@@ -22,7 +22,8 @@ Int <- function(x){
 #' @examples KNN_Python(c("Ama Dablam","Autumn","France","Climber",2025,1,1,1,1,1,1,1), n = 11 , p = 3)
 
 KNN_Python <- function(indiv_prediction,n = Int(11), p = Int(3)){
+  use_virtualenv("../Python/venv")
   source_python("../Python/main.py")
-  KNN_result = KNN_Process(data,indiv_prediction,target ,Int(n), Int(p))
+  KNN_result = KNN_Process(data,indiv_prediction,target, Int(p) ,Int(n))
   return(list(prediction = KNN_result[[1]], proba_f = KNN_result[[2]],proba_s = KNN_result[[3]],temps = KNN_result[[4]] ))
 }
